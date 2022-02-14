bff87fe3d0d5dad4a468dfca3abffffaecd08983
ilyAkagi <60408278+SirGavarine16@users.noreply.github.com>
2022-02-14 02:59:58 -0700

    Optimization for Inquirer questions.

--- src/helpers/inquirer.js
+++ src/helpers/inquirer.js
@@ -1,126 +1,44 @@
 const inquirer = require('inquirer')
-const Task = require('../models/task')
-require('colors')
+const { 
+    getMainMenuQuestions, 
+    getPauseQuestions, 
+    getReadInputQuestions, 
+    getConfirmationQuestions, 
+    getTaskToDeleteQuestions, 
+    getTasksToEditQuestions 
+} = require('./inquirer_questions')
 
-const mainMenuQuestions = [
-    {
-        type: 'list',
-        name: 'option',
-        message: 'What do you wish to do?',
-        choices: [
-            {
-                value: '1',
-                name: `${'[1]'.green} Show all tasks.`,
-            }, {
-                value: '2',
-                name: `${'[2]'.green} Show all pending tasks.`,
-            }, {
-                value: '3',
-                name: `${'[3]'.green} Show all completed tasks.`,
-            }, {
-                value: '4',
-                name: `${'[4]'.green} Create a new task.`,
-            }, {
-                value: '5',
-                name: `${'[5]'.green} Complete a task.`,
-            }, {
-                value: '6',
-                name: `${'[6]'.green} Delete a task.`,
-            }, {
-                value: '0',
-                name: `${'[0]'.green} Close.`,
-            },
-        ]
-    }
-]
 
 const getOptionFromInquirerMenu = async () => {
     console.clear()
-    const { option } = await inquirer.prompt(mainMenuQuestions)
-    return option
+    const { value } = await inquirer.prompt(getMainMenuQuestions())
+    return value
 }
 
 const pauseConsole = async () => {
-    const questions = [
-        {
-            type: 'input',
-            name: 'option',
-            message: `Press ${'ENTER'.green} to continue...`
-        }
-    ]
-    const { option } = await inquirer.prompt(questions)
-    return option
+    const { value } = await inquirer.prompt(getPauseQuestions())
+    return value
 }
 
-/**
- * @param {string} message 
- * @param {string} validation 
- */
 const readInput = async (message, validation) => {
-    const questions = [{
-        type: 'input',
-        name: 'value',
-        message,
-        validate(value) {
-            if (value.length === 0) {
-                return validation
-            }
-            return true
-        }
-    }]
-    const { value } = await inquirer.prompt(questions)
+    const { value } = await inquirer.prompt(getReadInputQuestions(message, validation))
     return value
 }
 
 const getConfirmation = async (message) => {
-    const questions = [{
-        type: 'confirm',
-        name: 'confirmation',
-        message
-    }]
-    const { confirmation } = await inquirer.prompt(questions)
-    return confirmation
+    const { value } = await inquirer.prompt(getConfirmationQuestions(message))
+    return value
 }
 
-/**
- * @param {Task[]} tasklist
- */
 const getTaskIDToDelete = async (tasklist) => {
-    const questions = [{
-        type: 'list',
-        name: 'value',
-        message: 'Delete',
-        choices: [
-            ...tasklist.map((task, index) => {
-                return {
-                    value: task.id,
-                    name: `${`[${index + 1}]`.green} ${task.description}`
-                }
-            }), 
-            { value: '0', name: `${`[0] Cancel`}` 
-        }]
-    }]
-    const { value } = await inquirer.prompt(questions)
+    const { value } = await inquirer.prompt(getTaskToDeleteQuestions(tasklist))
     return value
 }
 
 const getTaskIDsToEdit = async (tasklist) => {
-    const questions = [{
-        type: 'checkbox',
-        name: 'taskIDArray',
-        message: 'Select tasks',
-        choices: tasklist.map((task, index) => {
-            return {
-                value: task.id,
-                name: `${`[${index + 1}]`.green} ${task.description}`,
-                checked: task.completedAt !== null
-            }
-        })
-    }]
-    const { taskIDArray } = await inquirer.prompt(questions)
-    return taskIDArray
+    const { values } = await inquirer.prompt(getTasksToEditQuestions(tasklist))
+    return values
 }
-
 
 module.exports = {
     getOptionFromInquirerMenu,
---
+++ src/helpers/inquirer_questions.js
@@ -0,0 +1,129 @@
+const Task = require('../models/task')
+
+require('colors')
+
+/**
+ * @returns Inquirer questions with the options of the main menu.
+ */
+const getMainMenuQuestions = () => {
+    return [{
+        type: 'list',
+        name: 'value',
+        message: 'What do you wish to do?',
+        choices: [{
+            value: '1',
+            name: `${'[1]'.green} Show all tasks.`,
+        }, {
+            value: '2',
+            name: `${'[2]'.green} Show all pending tasks.`,
+        }, {
+            value: '3',
+            name: `${'[3]'.green} Show all completed tasks.`,
+        }, {
+            value: '4',
+            name: `${'[4]'.green} Create a new task.`,
+        }, {
+            value: '5',
+            name: `${'[5]'.green} Complete a task.`,
+        }, {
+            value: '6',
+            name: `${'[6]'.green} Delete a task.`,
+        }, {
+            value: '0',
+            name: `${'[0]'.green} Close.`,
+        }]
+    }]
+}
+
+/**
+ * @returns Inquirer questions with an input to catch an Enter.
+ */
+const getPauseQuestions = () => {
+    return [{
+        type: 'input',
+        name: 'value',
+        message: `Press ${'ENTER'.green} to continue...`
+    }]
+}
+
+/**
+ * @param {string} message The message to show on console.
+ * @param {string} validation The validation message to show on error on console.
+ * @returns Inquirer questions with an input to catch a validated value.
+ */
+const getReadInputQuestions = (message = '', validation = '') => {
+    return [{
+        type: 'input',
+        name: 'value',
+        message,
+        validate(value) {
+            if (value.length === 0) {
+                return validation
+            }
+            return true
+        }
+    }]
+}
+
+/**
+ * @param {string} message The message to show on console.
+ * @return Inquirer questions with a confirm.
+ */
+const getConfirmationQuestions = (message) => {
+    return [{
+        type: 'confirm',
+        name: 'value',
+        message
+    }]
+}
+
+/**
+ * @param {Task[]} tasklist List with all the tasks.
+ * @returns Inquirer questions with all the tasks to delete.
+ */
+const getTaskToDeleteQuestions = (tasklist) => {
+    return [{
+        type: 'list',
+        name: 'value',
+        message: 'Delete',
+        choices: [
+            ...tasklist.map((task, index) => {
+                return {
+                    value: task.id,
+                    name: `${`[${index + 1}]`.green} ${task.description}`
+                }
+            }),
+            {
+                value: '0', name: `${`[0]`.green} Cancel`
+            }
+        ]
+    }]
+}
+
+/**
+ * @param {Task[]} tasklist List with all the tasks.
+ * @returns Inquirer questions with all the tasks to edit.
+ */
+const getTasksToEditQuestions = (tasklist) => {
+    return [{
+        type: 'checkbox',
+        name: 'values',
+        message: 'Select tasks',
+        choices: tasklist.map((task, index) => {
+            return {
+                value: task.id,
+                name: `${`[${index + 1}]`.green} ${task.description}`,
+                checked: task.completedAt !== null
+            }
+        })
+    }]
+}
+
+module.exports = {
+    getMainMenuQuestions,
+    getPauseQuestions,
+    getReadInputQuestions,
+    getConfirmationQuestions,
+    getTaskToDeleteQuestions,
+    getTasksToEditQuestions
+}
